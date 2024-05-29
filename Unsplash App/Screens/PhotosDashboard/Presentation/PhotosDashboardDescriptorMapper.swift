1bd6e25ac5e44ca73a5113f35db4899d35baa0f4
Arman Galstyan <armangalstyan9597@gmail.com>
2024-05-30 02:18:51 +0400

    Add PhotosDashboardView screen

---
+++ Unsplash App/Screens/PhotosDashboard/Presentation/PhotosDashboardDescriptorMapper.swift
@@ -0,0 +1,8 @@
+//
+//  PhotosDashboardDescriptorMapper.swift
+//  Unsplash App
+//
+//  Created by Arman Galstyan on 30.05.24.
+//
+
+import Foundation
---
+++ Unsplash App/Screens/PhotosDashboard/Presentation/PhotosDashboardViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PhotosDashboardViewController.swift
+//  Unsplash App
+//
+//  Created by Arman Galstyan on 30.05.24.
+//
+
+import Foundation
+
+final class PhotosDashboardViewController {
+    
+}
